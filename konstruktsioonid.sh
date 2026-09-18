3b18755d77ce3e218099a2259c77f06054d2e6c4
gn5sqd99sy-design <gn5sqd99sy@privaterelay.appleid.com>
2026-09-18 23:37:19 +0300

    Lisatud teine Bash-skript

---
+++ konstruktsioonid.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Lisab funktsioonid failist functions.sh
+source /home/roman/skriptimine/functions.sh
+
+echo "=== Bash funktsioonide test ==="
+
+# Otsib .txt lõpuga faile
+otsi_txt_failid
+
+# Kontrollib, kas kasutaja on root
+kontrolli_root
